e41321be47c76987f5e99ea6a7bff0fe749f41a3
wsderte <alek81299@gmail.com>
2024-07-15 13:23:21 +0300

    angular 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { HttpClientModule } from '@angular/common/http'
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
 import { GetCurrencyService } from './services/getCurrency/getCurrency.service';
 import { ConverterComponent } from './components/converter/converter.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -19,42 +19,35 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatTabsModule } from '@angular/material/tabs';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    ConverterComponent,
-    FormComponent,
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MatButtonModule,
-    MatIconModule,
-    MatSlideToggleModule,
-    MatFormFieldModule,
-    MatSelectModule,
-    MatInputModule,
-    BrowserAnimationsModule,
-    MatTabsModule,
-    MatDialogModule,
-    MatToolbarModule,
-  ],
-  exports: [
-    MatIconModule,
-    MatButtonModule,
-    MatSlideToggleModule,
-    MatFormFieldModule,
-    MatSelectModule,
-    MatInputModule,
-    BrowserAnimationsModule,
-    MatTabsModule,
-    MatDialogModule,
-    MatToolbarModule,
-  ],
-  providers: [GetCurrencyService],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        HeaderComponent,
+        ConverterComponent,
+        FormComponent,
+    ],
+    exports: [
+        MatIconModule,
+        MatButtonModule,
+        MatSlideToggleModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        MatInputModule,
+        BrowserAnimationsModule,
+        MatTabsModule,
+        MatDialogModule,
+        MatToolbarModule,
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatButtonModule,
+        MatIconModule,
+        MatSlideToggleModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        MatInputModule,
+        BrowserAnimationsModule,
+        MatTabsModule,
+        MatDialogModule,
+        MatToolbarModule], providers: [GetCurrencyService, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
